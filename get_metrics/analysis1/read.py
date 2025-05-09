1dab7080da4b0c15ed8206a15fae43e760c505db
Philip Blackberg <philip.blackberg@monash.edu>
2025-05-09 13:50:36 +1000

    changes 3

---
+++ get_metrics/analysis1/read.py
@@ -0,0 +1,55 @@
+# == imports ==
+# -- Packages --
+import xarray as xr
+import numpy as np
+from pathlib import Path
+
+# -- util- and local scripts --
+import os
+import sys
+import importlib
+sys.path.insert(0, os.getcwd())
+def import_relative_module(module_name, file_path):
+    ''' import module from relative path '''
+    if file_path == 'utils':
+        cwd = os.getcwd()
+        if not os.path.isdir(os.path.join(cwd, 'utils')):
+            print('put utils folder in cwd')
+            print(f'current cwd: {cwd}')
+            print('exiting')
+            exit()
+        module_path = f"utils.{module_name}"        
+    else:
+        cwd = os.getcwd()
+        relative_path = os.path.relpath(file_path, cwd) # ensures the path is relative to cwd
+        module_base = os.path.dirname(relative_path).replace("/", ".").strip(".")
+        module_path = f"{module_base}.{module_name}"
+    return importlib.import_module(module_path)
+mS = import_relative_module('user_specs',                                           'utils')
+
+
+
+
+if __name__ == '__main__':
+    mS.get_user_specs(show = True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
