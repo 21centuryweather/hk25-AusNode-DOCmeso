515cc227229daaa380763909faeacfc741ce0207
Philip Blackberg <philip.blackberg@monash.edu>
2025-05-09 13:48:42 +1000

    changes2

---
+++ utils/util_calc/doc_metrics/L_org/L_org_globe.py
@@ -0,0 +1,90 @@
+'''
+# -----------------
+#     L_org
+# -----------------
+Calculation inspired by
+paper - https://journals.ametsoc.org/view/journals/atsc/80/12/JAS-D-23-0103.1.xml
+code - https://github.com/giobiagioli/organization_indices/blob/main/org_indices.py, also in: utils/util_calc/doc_metrics/L_org/L_org_rectangular.py
+
+'''
+
+# == imports ==
+# -- Packages --
+import os
+import sys
+import numpy as np
+import xarray as xr
+
+# -- imported scripts --
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+import utils.util_calc.distance_matrix.distance_matrix          as dM
+import utils.util_calc.distance_matrix                          as dM
+
+
+# == convective-core evaluation funcs ==
+def find_counts_in_radius_bins(lat_c, lon_c, lat_coords, lon_coords, r_bin_edges):
+    dist_c = dM.haversine_dist(np.array([lat_c] * len(lat_coords)), 
+                            np.array([lon_c] * len(lon_coords)), 
+                            np.array(lat_coords), 
+                            np.array(lon_coords))                                                                       # distance to other cores 
+    dist_c = dist_c[(dist_c > 0) & (dist_c < np.max(r_bin_edges))]                                                      # keep distances that are not distance from the core to itself, and the distances greater than r_max        
+    bin_indices = np.digitize(dist_c, r_bin_edges, right=True)                                                          # identifies associated bin (last bin edge is inclusive right, others not) (firt bin edge not inclusive left) (note: zero is outside range below, len(r_bins) is outside range above)
+    counts = np.array([np.sum(bin_indices == i) for i in range(1, len(r_bin_edges))])                                   # number of points in different r_bins
+    return np.cumsum(counts)  
+
+def find_weights(lat_c, lon_c, lat, r_bin_edges):
+    for r, r_dist in enumerate(r_bin_edges[1:]):                                                                        # first r_bins entry is zero
+        dist_to_y_edges = dM.haversine_dist(np.array([lat_c] * 2), 
+                                            np.array([lon_c] * 2), 
+                                            np.array([lat[0], lat[-1]]),                                                # top and bottom edge
+                                            np.array([lon_c] * 2))                                                      # distance from core to meridional edges    
+        area_circle_bin = np.pi * r_dist**2                                                                             # area of circle considered
+        area_inside     = np.pi * r_dist**2                                                                             # area of circle included in domain (updated in loop)
+        for i, edge_dist in enumerate(dist_to_y_edges):                                                                 # check both edges
+            if r_dist > edge_dist:                                                                                      # if the circle extends past the boundary
+                theta = 2 * np.arccos(edge_dist / r_dist)                                                               # angle of segment exceeding boundary (cosine rule: adjacent / hypo.)
+                area_circsec = (theta / 2) * r_dist**2                                                                  # circle sector area: (theta / 2pi) * pi r^2  
+                area_triangle = edge_dist * np.sqrt(r_dist**2 - edge_dist**2)                                           # triangle of circle segment (pyth. th and area of triangle * 2)   
+                area_inside -= (area_circsec - area_triangle)                                                           # subtract the area of the circle excluded due to the boundary
+        yield area_circle_bin / area_inside                                                                             # This weights the number of included points with a greater weight if part of the circle is outside the domain
+
+
+# == metric calc ==
+def get_L_org(conv_regions, da_area, r_bin_edges):
+    # -- identify cores --
+    lat, lon = conv_regions.lat.data, conv_regions.lon.data
+    I, J = zip(*np.argwhere(conv_regions.data))                                                                         # All spatial coordinates with convective points (i, j)
+    I, J = list(I), list(J)                                                                                             # index structured as (lat1, lon1), (lat1, lon2) ..
+    lat_coords, lon_coords = lat[I], lon[J]                                                                             # lat, lon coordinates of all cores
+    n_c = len(lat_coords)                                                                                               # number of "cores"
+
+    # -- core evaluation -- 
+    cum_counting = np.zeros((n_c, len(r_bin_edges) - 1))                                                                # considering bin edges (one less bin than bin edges)
+    weights = np.ones((n_c, len(r_bin_edges) - 1))                                                                      # This weights the number of included points with a greater weight if part of the circle is outside the domain
+    for c, (lat_c, lon_c) in enumerate(zip(lat_coords, lon_coords)):                                                    # position of selected core
+        cum_counting[c,:] = find_counts_in_radius_bins(lat_c, lon_c, lat_coords, lon_coords, r_bin_edges)               # how many cores fall in each circle radius
+        weights[c, :] = np.fromiter(find_weights(lat_c, lon_c, lat, r_bin_edges), dtype=float)                          # weight for each bin      
+    cum_counting_weighted = cum_counting * weights                                                                      # apply weights to the count
+
+    # -- Observational Bsag function (L^hat) -- 
+    Besag_obs = np.sqrt((da_area.sum().data / (np.pi * n_c * (n_c - 1))) * np.sum(cum_counting_weighted, axis = 0))     # one value for each r in r_bins
+    Besag_obs = np.concatenate(([0], Besag_obs))                                                                        # add zero at begining for integral
+
+    # -- Theoretical Bsag function (L) -- 
+    Besag_theor = r_bin_edges                                                                                           # linearly increasing with r
+
+    # -- L_org calc --                           
+    L_org = np.trapz(Besag_obs - Besag_theor, x = r_bin_edges)                                                          # integral between observed- and theoretical curve
+    L_ir = np.sqrt(np.trapz((Besag_obs - Besag_theor)**2, x = r_bin_edges))                                             # irregularity index (standard deviation between observed- and theoretical curve)
+    return L_org, L_ir, Besag_obs, Besag_theor, r_bin_edges
+
+
+# == when this script is ran ==
+if __name__ == '__main__':
+    ''
+
+
+
+
---
+++ utils/util_calc/doc_metrics/rome/rome.py
@@ -0,0 +1,99 @@
+'''
+# ------------
+#  util_calc
+# ------------
+
+'''
+
+# == imports ==
+# -- packages --
+import numpy as np
+import xarray as xr
+
+
+# -- imported scripts --
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+import utils.util_calc.connect_boundaries.contiguous_regions    as cR
+import utils.util_calc.distance_matrix.distance_matrix          as dM
+import utils.util_calc.area_weighting.globe_area_weight         as gW
+
+
+# == Calculate metric ==
+def calc_rome(da, distance_matrix = None, da_area = None, check_input = False):
+    '''         
+    rome = (1 / N) * sum(q_i(a, b)) for i = 1, 2, 3, n, when n > 1
+    rome = A_a                                          when n = 1
+    Where
+        q_i = A_a + min(1, A_b / A_d) * A_b 
+            A_a - larger area of unique pair
+            A_b - smaller area of unique pair
+            A_d - squared shortest distance between unique pair boundaries (km)
+        N = n
+    ref: https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2019JD031801
+    
+    Function takes:
+    da              - best: 2D binary xarray data array with coords = ['lat', 'lon'], elements: [0, 1], numpy array works too
+    distance_matrix - from utils
+    da_area         - from utils
+    '''
+    # -- check input --
+    if check_input:
+        # -- check elements --
+        if not np.array_equal(np.unique(da), [0, 1]):
+            print('check the input')
+            print('needs da with only [0, 1]')
+            print('exiting')
+            exit()
+        # -- check shape --
+        try:
+            lat_dim, lon_dim = np.shape(da)
+            if not lat_dim == len(da.lat) or not lon_dim == len(da.lon):
+                print('check the input')
+                print('needs da in shape [len(lat), len(lon)]')
+                print('exiting')
+                exit()                
+        except:
+            print('check the input')
+            print('needs da as xarray object with [lat, lon]')
+            print('exiting')
+            exit()
+
+    # -- contiguous regions --
+    da, labels = cR.get_contiguous_regions(da, connect_lon = True)                  # each object gets a unique number specified by labels
+
+    # -- distance matrix --
+    if distance_matrix is None:
+        distance_matrix = dM.create_distance_matrix(da.lat, da.lon)                 # quicker to define this before, and give to functino
+
+    # -- area_weghting --
+    if da_area is None:
+        da_area = gW.get_area_matrix(da.lat, da.lon)                                # quicker to define this before, and give to functino
+
+    # -- rome --
+    if not len(labels) == 1:
+        # -- q-weights --
+        weights = []
+        for i, label_i in enumerate(labels[0:-1]): 
+            obj_i = da.isin(label_i)                                                # has the label number
+            obj_i = xr.where(obj_i > 0, 1, 0)                                       # [0, 1]
+            obj_i_dist_to = dM.find_distance(obj_i, distance_matrix)                # distance from obj_i to rest of domain
+            obj_i_area = (obj_i * da_area).sum()                                    # area of object
+            for _, label_j in enumerate(labels[i+1:]):
+                obj_j = da.isin(label_j)
+                obj_j = xr.where(obj_j > 0, 1, np.nan)
+                obj_j_area = (obj_j * da_area).sum()
+                A_a, A_b = sorted([obj_i_area, obj_j_area], reverse=True)
+                A_d = (obj_i_dist_to * obj_j).min()**2                              # squared closest distance to obj_j
+                weights.append(A_a + np.minimum(1, A_b / A_d) * A_b)
+        return np.mean(np.array(weights))
+    else:
+        return np.sum((da.isin(labels) > 0) * 1 * da_area)                          # area of object
+
+
+# == when this script is ran ==
+if __name__ == '__main__':
+    ''
+
+
